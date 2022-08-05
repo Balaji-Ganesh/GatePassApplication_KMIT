6ed2ef942336d234aede05c9fc1a6eb21b4cb298
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-08-05 20:48:05 +0530

    added secret keys to the environment variables

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const firebase = require("firebase");
+require("dotenv").config();
 const firebaseConfig = {
   // ------ used for the testing..
   // apiKey: "AIzaSyBIDUGA_aL0Xe48g4-AJLEPFd6dy9I5b2k",
@@ -9,12 +10,12 @@ const firebaseConfig = {
   // appId: "1:309754976933:web:48cdbf65eb2cd4ff9f0b4c",
 
   // --- actual real-time database..
-  apiKey: "AIzaSyDMvoq-_USWQbmAhFzfePivOTlTqHQvBdg",
-  authDomain: "gatepass-b7114.firebaseapp.com",
-  projectId: "gatepass-b7114",
-  storageBucket: "gatepass-b7114.appspot.com",
-  messagingSenderId: "380433476859",
-  appId: "1:380433476859:web:9727a555ef85d51ef1d45e",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
